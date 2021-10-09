e6a77de6753e54f4e6da192fb88536adb78569c0
Arthur Augsten <arthuraugsten@gmail.com>
2021-10-09 04:17:58 -0300

    ✨ #215 - Added notification object

---
+++ src/Augsten.NotificationContext/Notification.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Augsten.NotificationContext
+{
+    /// <summary>
+    /// The object represents a notification for an error state during the request.
+    /// </summary>
+    public sealed class Notification
+    {
+        /// <summary>
+        /// Construct a notification object representing an error with the specified message.
+        /// </summary>
+        /// <param name="message">Specify an error message for the notification.</param>
+        public Notification([AllowNull] string? message)
+            => Message = message;
+
+        /// <summary>
+        /// Construct a notification object representing an error with the code and message specified.
+        /// </summary>
+        /// <param name="code">Specify an error code for the notification.</param>
+        /// <param name="message">Specify an error message for the notification.</param>
+        public Notification([AllowNull] string? code, [AllowNull] string? message) : this(message)
+            => Code = code;
+
+        /// <summary>
+        /// Represents a code for the error.
+        /// </summary>
+        public string? Code { get; }
+
+        /// <summary>
+        /// Represents a message for the error.
+        /// </summary>
+        public string? Message { get; }
+    }
+}
---
+++ tests/Augsten.NotificationContext.UnitTests/NotificationTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Augsten.NotificationContext.UnitTests
+{
+    public sealed class NotificationTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        [InlineData("abc")]
+        public void Should_build_correctly_when_passing_message_only(string message)
+        {
+            var notification = new Notification(message);
+            notification.Message.Should().Be(message);
+        }
+
+        [Theory]
+        [InlineData("", "abc")]
+        [InlineData(" ", null)]
+        [InlineData(null, " ")]
+        [InlineData("abc", "")]
+        public void Should_build_correctly_when_passing_code_and_message(string code, string message)
+        {
+            var notification = new Notification(code, message);
+            notification.Should().BeEquivalentTo(new { Code = code, Message = message });
+        }
+    }
+}
