2aade39cc615c55dac1db00cc749dc7610f6fd6b
Arthur Augsten <arthuraugsten@gmail.com>
2021-10-11 23:45:12 -0300

    ✨ #9 - Added pipeline integrated with FluentValidation

---
+++ src/Augsten.Notifications.Web/PipelineValidator.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Augsten.Notifications.Web
+{
+    /// <summary>
+    /// This class is a middleware to integrate with MediatR. It will be executed before the execution
+    /// of IRequestHandler<<typeparamref name="TRequest"/>, <typeparamref name="TResponse"/>>.
+    /// This validator is integrated with the FluentValidation library. It will require all validators registered
+    /// in dependency injection container, if the application has validators registered, it will execute them, otherwise, will ignore execute the next <see cref="RequestHandlerDelegate{TResponse?}"/>.
+    /// All error encoutered in the validation execution will be added in the <see cref="NotificationContext">.
+    /// </summary>
+    /// <typeparam name="TRequest">An object with an inheritance of IRequest<T></typeparam>
+    /// <typeparam name="TResponse">An object of type T</typeparam>
+    public sealed class PipelineValidator<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse?> where TRequest : IRequest<TResponse?>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+        private readonly INotificationContext _notificationContext;
+
+        public PipelineValidator(IEnumerable<IValidator<TRequest>> validators, INotificationContext notificationContext)
+        {
+            _validators = validators;
+            _notificationContext = notificationContext;
+        }
+
+        public async Task<TResponse?> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse?> next)
+        {
+            _ = next ?? throw new ArgumentNullException(nameof(next));
+            var context = new ValidationContext<TRequest>(request);
+
+            foreach (var validator in _validators ?? Array.Empty<IValidator<TRequest>>())
+            {
+                if (await validator.ValidateAsync(context) is { IsValid: false } validationResult)
+                {
+                    foreach (var error in validationResult.Errors.Where(t => t is not null))
+                        _notificationContext.Add(error.ErrorMessage);
+
+                    return default;
+                }
+            }
+
+            return await next();
+        }
+    }
+}
---
+++ tests/Augsten.Notifications.Web.UnitTests/PipelineValidatorTests.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Augsten.Notifications.Web.UnitTests
+{
+    public sealed class PipelineValidatorTests
+    {
+        private PipelineValidator<CommandMock, CommandResultMock> _pipelineValidator;
+        private IEnumerable<IValidator<CommandMock>> _validators;
+
+        private readonly Mock<INotificationContext> _notificationContext = new();
+        private readonly Mock<RequestHandlerDelegate<CommandResultMock>> _delegate = new();
+        private readonly CommandMock _request = new("Teste");
+
+        public static List<object[]> CenarioValidadores { get; } = new(2)
+        {
+            new object[] { Enumerable.Empty<IValidator<CommandMock>>() },
+            new object[] { default(List<IValidator<CommandMock>>) }
+        };
+
+        [Fact]
+        public async Task Should_throw_exception_when_next_request_delegate_is_null()
+        {
+            _validators = Enumerable.Empty<IValidator<CommandMock>>();
+            _pipelineValidator = new PipelineValidator<CommandMock, CommandResultMock>(_validators, _notificationContext.Object);
+
+            var excecao = await Assert.ThrowsAsync<ArgumentNullException>(async () => await _pipelineValidator.Handle(_request, default, default));
+
+            excecao.Message.Should().Be("Value cannot be null. (Parameter 'next')");
+            _notificationContext.VerifyNoOtherCalls();
+            _delegate.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(CenarioValidadores))]
+        public async Task Should_return_success_when_doesnt_have_validators_registered(IEnumerable<IValidator<CommandMock>> validadores)
+        {
+            var resultadoEsperado = new CommandResultMock();
+
+            _validators = Enumerable.Empty<IValidator<CommandMock>>();
+            _delegate.Setup(t => t.Invoke()).ReturnsAsync(resultadoEsperado);
+            _pipelineValidator = new PipelineValidator<CommandMock, CommandResultMock>(validadores, _notificationContext.Object);
+
+            var resultado = await _pipelineValidator.Handle(_request, default, _delegate.Object);
+
+            _notificationContext.VerifyNoOtherCalls();
+            _delegate.Verify(t => t.Invoke(), Times.Once);
+
+            resultado.Should().BeSameAs(resultadoEsperado);
+        }
+
+        [Fact]
+        public async Task Should_return_null_when_validation_fails()
+        {
+            _validators = new[] { new CommandMockValidation() };
+
+            _pipelineValidator = new PipelineValidator<CommandMock, CommandResultMock>(_validators, _notificationContext.Object);
+
+            var resultado = await _pipelineValidator.Handle(new(string.Empty), default, _delegate.Object);
+
+            _notificationContext.Verify(t => t.Add(CommandMockValidation.Mensagem), Times.Once);
+            _delegate.VerifyNoOtherCalls();
+
+            resultado.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Should_return_the_request_delegate_result_when_validation_doest_fail()
+        {
+            var resultadoEsperado = new CommandResultMock();
+
+            _validators = new[] { new CommandMockValidation() };
+            _delegate.Setup(t => t.Invoke()).ReturnsAsync(resultadoEsperado);
+            _pipelineValidator = new PipelineValidator<CommandMock, CommandResultMock>(_validators, _notificationContext.Object);
+
+            var resultado = await _pipelineValidator.Handle(_request, default, _delegate.Object);
+
+            _notificationContext.VerifyNoOtherCalls();
+            _delegate.Verify(t => t.Invoke(), Times.Once);
+            resultado.Should().BeSameAs(resultadoEsperado);
+        }
+
+        public sealed record CommandMock(string Name) : IRequest<CommandResultMock>;
+        public sealed record CommandResultMock { }
+
+        public sealed class CommandMockValidation : AbstractValidator<CommandMock>
+        {
+            public const string Mensagem = "Message";
+
+            public CommandMockValidation()
+                => RuleFor(t => t.Name).NotEmpty().WithMessage(Mensagem);
+        }
+    }
+}
